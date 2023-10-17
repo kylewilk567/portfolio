2b6275a040d75c89bf9cf68956f9ec162a36ca55
kylewilk <kylewilk@umich.edu>
2023-10-16 21:06:32 -0400

    Add previous state tracking

--- components/PageContent.jsx
+++ components/PageContent.jsx
@@ -3,35 +3,41 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion"; // Import motion and AnimatePresence
 import About from "./About";
 import Contact from "./Contact";
+import Main from "./Main";
 
 const PageContent = () => {
-  const [activeSection, setActiveSection] = useState("about");
+  const [activeSection, setActiveSection] = useState("");
   const [previousSection, setPreviousSection] = useState(null);
+
+  const handleHashChange = () => {
+    // Get the current hash and update the activeSection state
+    const hash = window.location.hash.slice(1); // remove the '#' sign
+    setActiveSection((prevActiveSection) => {
+      // Use the previous state to calculate the new state
+      setPreviousSection(prevActiveSection);
+      return hash; // Update activeSection based on the new hash
+    });
+    setActiveSection(hash);
+  };
 
   useEffect(() => {
     // Listen to changes in the URL hash
     window.addEventListener("hashchange", handleHashChange);
     // Initial check of the hash
-    handleHashChange();
+    setActiveSection(window.location.hash.slice(1));
 
     return () => {
       window.removeEventListener("hashchange", handleHashChange);
     };
   }, []);
 
-  const handleHashChange = () => {
-    // Get the current hash and update the activeSection state
-    const hash = window.location.hash.slice(1); // remove the '#' sign
-    setActiveSection(hash);
-  };
-
-  // TODO: Track previous state to be used to select existing content before sliding over
   // TODO: Ensure the new content covers fully the old content
   return (
     <div style={{ position: "relative" }}>
       <div style={{ position: "absolute", width: "100%" }}>
-        <About />
-        <Contact />
+        {previousSection === "" && <Main />}
+        {previousSection === "about" && <About />}
+        {previousSection === "contact" && <Contact />}
       </div>
       <AnimatePresence mode="wait">
         <motion.div
@@ -42,6 +48,7 @@ const PageContent = () => {
           //   exit={{ x: '-100%' }}
           transition={{ type: "tween", duration: 0.5 }}
         >
+          {activeSection === "" && <Main />}
           {activeSection === "about" && <About />}
           {activeSection === "contact" && <Contact />}
         </motion.div>
