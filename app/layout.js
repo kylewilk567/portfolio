cbb01366ad6970f91df916c6e4a7fe21a29794f4
kylewilk <kylewilk@umich.edu>
2023-11-01 21:14:35 -0400

    Fix theme providers placement - fixes console errors

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Providers>
-        <body>{children}</body>
-      </Providers>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
