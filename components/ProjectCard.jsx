5890e7ec378228d14925df5002bcbd0dcbee47c9
kylewilk <kylewilk@umich.edu>
2023-11-25 11:40:45 -0500

    Fix project page link appearing over tech link

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,14 +5,17 @@ import Image from "next/image";
 
 const ProjectCard = ({ project }) => {
   return (
-    <a href={`#${project.href}`}>
-      <div className="drop-shadow-md hover:drop-shadow-xl brand-nav-bg-color w-64 h-128 rounded-md overflow-hidden">
+    <div className="drop-shadow-md hover:drop-shadow-xl brand-nav-bg-color w-64 h-128 rounded-md overflow-hidden flex flex-col">
+      <a className="flex-grow" href={`#${project.href}`}>
         <div className="w-full h-64 relative">
           <Image
             src={project.image}
             alt="image"
-            layout="fill"
-            objectFit="cover"
+            // layout="fill"
+            fill
+            // objectFit="cover"
+            sizes="100vw"
+            className="object-cover"
           />
         </div>
 
@@ -20,16 +23,15 @@ const ProjectCard = ({ project }) => {
           <h2 className="font-bold text-center text-base">{project.name}</h2>
           <h3 className="text-sm">{project.description}</h3>
         </div>
-
-        <div className="absolute bottom-0 p-1 w-full bg-slate-300">
-          <div className="flex gap-2 items-center">
-            {project.technologies.map((technology) => (
-              <TechIcon key={technology.text} technology={technology} />
-            ))}
-          </div>
+      </a>
+      <div className="h-10 p-1 w-full bg-slate-300">
+        <div className="flex gap-2 items-center">
+          {project.technologies.map((technology) => (
+            <TechIcon key={technology.text} technology={technology} />
+          ))}
         </div>
       </div>
-    </a>
+    </div>
   );
 };
 
