5f17720b9e9fd218df65879e7b91976305dd62bf
kylewilk <kylewilk@umich.edu>
2023-12-23 13:53:47 -0500

    Fix aurora

--- app/page.js
+++ app/page.js
@@ -14,14 +14,19 @@ export default function Home() {
     //   </div>
     // </div>
     <div className="bg-white flex justify-center">
-      <div className="max-w-7xl w-full relative flex">
+      <div className="absolute overflow-hidden w-full h-full xl:block hidden">
+        <div className="base one"></div>
+        <div className="base two"></div>
+        <div className="base three"></div>
+      </div>
+      <div className="max-w-7xl w-full relative z-10 flex">
         <Nav />
         <div className="mt-12 sm:mt-0 max-w-[1104px] w-full min-h-screen bg-white items-center border-r-2">
-          {/* <div className="absolute overflow-hidden w-full h-full">
+          <div className="absolute overflow-hidden w-full max-w-[1104px] h-full xl:hidden">
             <div className="base one"></div>
             <div className="base two"></div>
             <div className="base three"></div>
-          </div> */}
+          </div>
           <div className="relative z-20">
             <PageContent />
           </div>
