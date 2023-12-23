64c4a7c7fb6731caad0d8c1eb29d775ff5ee35be
kylewilk <kylewilk@umich.edu>
2023-12-23 12:48:49 -0500

    Fix content shift when scrollbar appears

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     //     </div>
     //   </div>
     // </div>
-    <div className="bg-white justify-center">
+    <div className="bg-white flex justify-center">
       <div className="max-w-7xl w-full relative flex">
         <Nav />
         <div className="mt-12 sm:mt-0 max-w-[1104px] w-full min-h-screen bg-white items-center border-r-2">
--- components/PageContent.jsx
+++ components/PageContent.jsx
@@ -10,80 +10,18 @@ import Pathfinding from "./Pathfinding";
 import MTouch from "./MTouch";
 
 const PageContent = () => {
-  const previousSectionRef = useRef(null);
-  let { currentHash: activeSection, previousHash: previousSection } = useHash();
+  let { currentHash: activeSection } = useHash();
   activeSection = activeSection.slice(1);
-  previousSection = previousSection.slice(1);
-
-  const handleAnimationStart = () => {
-    window.scrollTo(0, 0);
-    // if (previousSectionRef.current) {
-    //   if (previousSectionRef.current.style)
-    //     previousSectionRef.current.style.display = "block";
-    //   else previousSectionRef.current.style = { display: "block" };
-    // }
-  };
-
-  const handleAnimationComplete = () => {
-    if (previousSectionRef.current) {
-      if (previousSectionRef.current.style)
-        previousSectionRef.current.style.display = "none";
-      else previousSectionRef.current.style = { display: "none" };
-    }
-  };
 
   return (
-    <>
-      <div className="w-full min-h-screen brand-bg-color px-10">
-        {activeSection === "" && <Main />}
-        {activeSection === "about" && <About />}
-        {activeSection === "projects" && <Projects />}
-        {activeSection === "contact" && <Contact />}
-        {activeSection === "pathfinding" && <Pathfinding />}
-        {activeSection === "mtouch" && <MTouch />}
-      </div>
-      {/* <div
-        id="previous"
-        className="w-full min-h-screen brand-bg-color px-10"
-        ref={previousSectionRef}
-      >
-        {previousSection === "" && <Main />}
-        {previousSection === "about" && <About />}
-        {previousSection === "projects" && <Projects />}
-        {previousSection === "contact" && <Contact />}
-        {previousSection === "pathfinding" && <Pathfinding />}
-        {previousSection === "mtouch" && <MTouch />}
-      </div> */}
-
-      {/* <AnimatePresence mode="wait">
-        <motion.div
-          className="w-full min-h-screen brand-bg-color px-10"
-          key={activeSection}
-          initial={{ x: "100%" }}
-          // whileInView={{
-          //   x: 0,
-          //   y: 0,
-          //   transition: {
-          //     type: "tween",
-          //     delay: 0,
-          //     duration: 0.5,
-          //     ease: "easeOut",
-          //   },
-          // }}
-          animate={{ x: 0 }}
-          transition={{ type: "tween", duration: 0.5 }}
-          onAnimationStart={handleAnimationStart}
-          onAnimationComplete={handleAnimationComplete}
-        >
-          {activeSection === "" && <Main />}
-          {activeSection === "about" && <About />}
-          {activeSection === "projects" && <Projects />}
-          {activeSection === "contact" && <Contact />}
-          {activeSection === "pathfinding" && <Pathfinding />}
-          {activeSection === "mtouch" && <MTouch />}
-        </motion.div>
-      </AnimatePresence> */}
-    </>
+    <div className="w-full min-h-screen brand-bg-color px-10">
+      {activeSection === "" && <Main />}
+      {activeSection === "about" && <About />}
+      {activeSection === "projects" && <Projects />}
+      {activeSection === "contact" && <Contact />}
+      {activeSection === "pathfinding" && <Pathfinding />}
+      {activeSection === "mtouch" && <MTouch />}
+    </div>
   );
 };
 
