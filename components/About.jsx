6773b5fdeb112aae47c8d95a4628b77cbc948ca3
kylewilk <kylewilk@umich.edu>
2023-12-23 21:33:23 -0500

    Align Education with Experiences section

--- components/About.jsx
+++ components/About.jsx
@@ -12,20 +12,18 @@ const About = () => {
         <Header title="About Me" />
         <div className="grid lg:grid-cols-3">
           <div className="lg:col-span-2 flex flex-col">
-            <div className="flex flex-col">
+            <div className="flex flex-col w-[95%]">
               <h2 className="font-bold text-2xl my-4">Education</h2>
-              <div className="shadow-[0_3px_0_#ddd] brand-nav-bg-color rounded-md overflow-hidden flex flex-col p-8 w-[95%] max-w-[1170px]">
-                <h1 className="text-2xl font-bold flex">
+              <div className="shadow-[0_3px_0_#ddd] brand-nav-bg-color rounded-md flex flex-col p-4 ml-[60px] max-w-[1170px]">
+                <h1 className="text-2xl font-bold">
                   University of Michigan - College of Engineering
                 </h1>
-                <h2 className="text-lg text-gray-600 flex">Ann Arbor, MI</h2>
-                <p className="mb-2 flex">May 2023</p>
-                <p className="mb-2 flex">
+                <h2 className="text-lg text-gray-600">Ann Arbor, MI</h2>
+                <p className="mb-2">May 2023</p>
+                <p className="mb-2">
                   B.S.E. in Computer Science, Minor in Electrical Engineering
                 </p>
-                <p className="text-green-600 font-semibold flex">
-                  Summa Cum Laude
-                </p>
+                <p className="text-green-600 font-semibold">Summa Cum Laude</p>
               </div>
             </div>
             <div className="flex flex-col">
