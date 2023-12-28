eccc81e03daa939af5e8a450c7988137edb54c3f
kylewilk <kylewilk@umich.edu>
2023-12-27 20:52:35 -0500

    Delete old code

--- app/page.js
+++ app/page.js
@@ -3,16 +3,6 @@ import PageContent from "@/components/PageContent";
 
 export default function Home() {
   return (
-    // <div className="bg-white justify-center flex">
-    //   <div className="max-w-7xl w-full relative flex flex-[1_0_auto]">
-    //     <div className="h-screen w-44 sticky top-0 z-20 flex-shrink-0 brand-nav-bg-color hidden sm:block pt-6">
-    //       Sidebar MORE STUFF MORE STUFF MORE STUFF MORE STUFF
-    //     </div>
-    //     <div className="mt-12 sm:mt-0 w-[1104px] min-h-screen bg-white items-center">
-    //       Content
-    //     </div>
-    //   </div>
-    // </div>
     <div className="brand-bg-color flex justify-center">
       <div className="absolute overflow-hidden w-full h-full xl:block hidden">
         <div className="base one"></div>
